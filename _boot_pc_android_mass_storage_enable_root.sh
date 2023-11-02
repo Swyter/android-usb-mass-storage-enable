3aaae1f0ff1cbe3b664ac47d6725b09c0a881d36
Swyter <swyterzone@gmail.com>
2023-11-02 23:52:20 +0100

    Retouch a bit the script, to use cd instead of the unsupported pushd and make the read pause actually work after printing the user-facing text, once they are done with the virtual drive.

--- _boot_pc_android_mass_storage_enable_root.sh
+++ _boot_pc_android_mass_storage_enable_root.sh
@@ -13,7 +13,7 @@
 CONFIGFS=`mount -t configfs | head -n1 | cut -d' ' -f 3`
 
 mkdir $CONFIGFS/usb_gadget/swy # swy: create a new gadget
-pushd $CONFIGFS/usb_gadget/swy # swy: enter the folder
+cd    $CONFIGFS/usb_gadget/swy # swy: enter the folder
 
 echo 0x1337 > idVendor  # swy: set the USB manufacturer code
 echo 0x1337 > idProduct # swy: set the USB device code
@@ -36,13 +36,15 @@ echo "y"                                   > functions/mass_storage.0/lun.0/ro
 echo "y"                                   > functions/mass_storage.0/lun.0/removable
 echo "y"                                   > functions/mass_storage.0/lun.0/cdrom
 echo "/sdcard/Download/netboot.xyz.iso"    > functions/mass_storage.0/lun.0/file # swy: make sure we assign the actual path last, or setting ro/cdrom won't work until we empty this
+
 ln -s functions/mass_storage.0 configs/swyconfig.1 # swy: add a symbolic link to put our function into a premade config folder
 
 # swy: enable/attach the gadget to the physical USB controller; mark this gadget as active
 getprop sys.usb.controller > UDC
 setprop sys.usb.state mass_storage
 
-read "[i] press any key to exit the mass storage gadget mode"
+echo "[i] press any key to exit the mass storage gadget mode..." && read
+echo 
 
 # swy: dettach the gadget
 echo "" > UDC
